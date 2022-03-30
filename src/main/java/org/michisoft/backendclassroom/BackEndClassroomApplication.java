0bba89ba4a6170124edb8704d059418bdc02874a
Jose Maria <josmariaguilar@gmail.com>
2022-03-29 22:48:04 -0400

    first commit back-end

---
+++ src/main/java/org/michisoft/backendclassroom/BackEndClassroomApplication.java
@@ -0,0 +1,13 @@
+package org.michisoft.backendclassroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndClassroomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndClassroomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/michisoft/backendclassroom/BackEndClassroomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.michisoft.backendclassroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndClassroomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
